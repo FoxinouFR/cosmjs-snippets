275386fc50108939729836612a82064adcef576d
Foxinou <37015399+FoxinouFR@users.noreply.github.com>
2023-11-19 11:03:26 +0100

    Update valcons.js

--- addresses/valcons.js
+++ addresses/valcons.js
@@ -14,14 +14,11 @@ const rpcEndpoint = "https://rpc.cosmos.directory/bitsong";
 const validatorAddress = "bitsongvaloper1cqg5qc5zce7rkga8cwdyk6g55jnsweyrt3lw7v";
 const valconsPrefix = "bitsongvalcons";
 
-// Query client
 const tmClient = await Tendermint37Client.connect(rpcEndpoint);
 const queryClient = QueryClient.withExtensions(tmClient, setupStakingExtension);
 
-// Obtenir la clef de consensus publique
 const validator = await queryClient.staking.validator(validatorAddress);
 
-// Convertir consensusPubkey en adresse
 const pubkey = decodePubkey(validator.validator.consensusPubkey);
 const address8Array = pubkeyToRawAddress("ed25519", fromBase64(pubkey.value));
 const valconsAddress = toBech32(valconsPrefix, address8Array);
